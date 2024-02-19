203137ab751a6e332530e88b262008589370ebc2
BuildTools <unconfigured@null.spigotmc.org>
2024-02-18 17:49:07 -0700

    Add a module template

---
+++ module.h
@@ -0,0 +1,14 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+// Function pointer type
+typedef int (*module_function_t)(int, char**);
+
+// Module struct type
+typedef struct {
+    char long_name[50];
+    char short_name[5];
+    module_function_t function;
+} module_t;
+
+#endif
