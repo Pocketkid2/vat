3148190e287aa4cf1b5e3f7394a8bdf5bae4e1e9
BuildTools <unconfigured@null.spigotmc.org>
2024-02-24 10:35:07 -0700

    Move utility header to its own include directory

--- analyze_audio_track/analyze_audio_track.h
+++ analyze_audio_track/analyze_audio_track.h
@@ -1,7 +1,7 @@
 #ifndef VAT_ANALYZE_AUDIO_TRACK_H
 #define VAT_ANALYZE_AUDIO_TRACK_H
 
-#include "../module.h"
+#include "module.h"
 
 extern module_t analyze_audio_track_module;
 
--- analyze_color_space/analyze_color_space.h
+++ analyze_color_space/analyze_color_space.h
@@ -1,7 +1,7 @@
 #ifndef VAT_ANALYZE_COLOR_SPACE_H
 #define VAT_ANALYZE_COLOR_SPACE_H
 
-#include "../module.h"
+#include "module.h"
 
 extern module_t analyze_color_space_module;
 
