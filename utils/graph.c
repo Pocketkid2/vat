93e588d0ed7f0b767960c6862e1127bb1f9d5428
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 17:47:44 -0700

    Make white background and margin

--- utils/graph.c
+++ utils/graph.c
@@ -1,16 +1,26 @@
 #include "graph.h"
 
 void create_graph(double* data, int data_count, int width, int height, const char* filename) {
+    // Define margin
+    int margin = (width < height) ? width / 10 : height / 10;
+
     // Calculate the number of data points per pixel
-    int points_per_pixel = data_count / width;
+    int points_per_pixel = data_count / (width - 2 * margin);
     if (points_per_pixel == 0) points_per_pixel = 1;
 
     // Create the graph
     cairo_surface_t* surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
     cairo_t* cr = cairo_create(surface);
 
+    // Set background to white
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_paint(cr);
+
+    // Set line color to black
+    cairo_set_source_rgb(cr, 0, 0, 0);
+
     // Draw graph
-    for (int i = 0; i < width; i++) {
+    for (int i = 0; i < width - 2 * margin; i++) {
         // Calculate the average of the data points for this pixel
         double sum = 0;
         for (int j = 0; j < points_per_pixel; j++) {
@@ -19,14 +29,14 @@ void create_graph(double* data, int data_count, int width, int height, const cha
         double average = sum / points_per_pixel;
 
         // Draw the point on the graph
-        cairo_line_to(cr, i, height - (average * height));
+        cairo_line_to(cr, i + margin, height - margin - (average * (height - 2 * margin)));
     }
     cairo_stroke(cr);
 
     // Label axes
-    cairo_move_to(cr, 0, height - 10);
+    cairo_move_to(cr, margin, height - margin - 10);
     cairo_show_text(cr, "0");
-    cairo_move_to(cr, width - 10, height - 10);
+    cairo_move_to(cr, width - margin - 10, height - margin - 10);
     cairo_show_text(cr, "1");
 
     // Save to PNG
